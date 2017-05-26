da850518a4f688894578ca5bfdcdb19dc98ec591
lucaspinheiro7 <lucaspinheiro7@hotmail.com>
2017-05-26 13:14:25 -0300

    Configuração Spring

--- ProjetoWeb/src/main/java/br/com/fametro/model/dao/AdministradorDAO.java
+++ ProjetoWeb/src/main/java/br/com/fametro/model/dao/AdministradorDAO.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.fametro.model.entity.Administrador;
-import br.com.fametro.model.util.JPAUtil;
 
 @Repository
 public class AdministradorDAO {
@@ -15,10 +14,6 @@ public class AdministradorDAO {
 	// Injeção de dependência do Entity Manager
 	@PersistenceContext
 	EntityManager em;
-	
-	public AdministradorDAO(){
-		em = JPAUtil.abreConexao();
-	}
 	 
 	@Transactional
 	public void salvar(Administrador administrador){
